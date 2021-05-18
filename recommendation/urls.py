ccb8ec77e3e40085131120b5af0254fad6f37dea
The-KS101 <sesantakuro29@gmail.com>
2021-05-18 22:49:25 +0100

    pleaseee

--- recommendation/urls.py
+++ recommendation/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('similarGames/<str:game>/filter?<str:console>', views.simGames, name="similar_games"),
-    path('similarGames/<str:game>/filter?<str:console>/sort?<str:sortBy>&<str:ordChoice>', views.simGamesSorted, name="similar_games_sorted"),
+
 ]
