d30833d38f33c1f0978cf7d13fdd1d85330408bb
The-KS101 <sesantakuro29@gmail.com>
2021-05-19 03:09:47 +0100

    pleaseee

--- unnamed/settings.py
+++ unnamed/settings.py
@@ -128,7 +128,8 @@ AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 # s3 static settings
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+AWS_LOCATION = 'static'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
