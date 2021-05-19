7c30795844d0a768fae831a5a5f9b82b49565895
The-KS101 <sesantakuro29@gmail.com>
2021-05-19 13:38:47 +0100

    Multithreaded approach

--- recommendation/GetSimilar.py
+++ recommendation/GetSimilar.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import time
+import threading
 
 dataset = pd.read_csv('MetaCritic All games Gen 4 Draft 2.csv')
 
@@ -43,17 +44,17 @@ def getRecommend(title, dataset=dataset, console=None, sort=None, order=None):
     names, idx = get_name(title)
     cv = CountVectorizer(stop_words='english')
     vec_matrix = cv.fit_transform(dataset['combined words'])
-    vec_matrix = vec_matrix.A
+
     gameShown = dataset['names'][idx]
     if console != None:
         eligible_index = dataset[dataset['platforms'] == console].index
-        sims = cosine_sim_row(vec_matrix[idx], vec_matrix)
+        sims = thread_val(vec_matrix[idx], vec_matrix)
         print(f"look")
         mostSimIndex = np.argsort(sims)
         mostSimElig = [i for i in mostSimIndex if i in eligible_index]
         mostSim = mostSimElig[:: -1][1:30]
     else:
-        sims = cosine_sim_row(vec_matrix[idx], vec_matrix)
+        sims = thread_val(vec_matrix[idx], vec_matrix)
         mostSimIndex = np.argsort(sims)
         mostSim = mostSimIndex[:: -1][1:30]
         print("we are here")
@@ -72,12 +73,35 @@ def getRecommend(title, dataset=dataset, console=None, sort=None, order=None):
 
     return similarGames, gameShown
 
-def cosine_sim_row(m, n):
+def cosine_sim_row(m, n, sims):
+    m = np.squeeze(np.asarray(m.A))
+    sims.append([np.dot(m, np.squeeze(np.asarray(i.A)))/(np.linalg.norm(m)*np.linalg.norm(np.squeeze(np.asarray(i.A)))) for i in n ])
+
+
+def thread_val(row_data, vec_matrix):
     start = time.time()
-    print(type(m))
-    sims = [np.dot(m, i)/(np.linalg.norm(m)*np.linalg.norm(i)) for i in n ]
+    threads = []
+    mat_1 = vec_matrix[0:1639]
+    mat_2 = vec_matrix[1639:3278]
+    mat_3 = vec_matrix[3278:4647]
+    mat_4 = vec_matrix[4647:]
+    sims = []
+    t1 = threading.Thread(target=cosine_sim_row, args=(row_data, mat_1, sims))
+    t2 = threading.Thread(target=cosine_sim_row, args=(row_data, mat_2, sims))
+    t3 = threading.Thread(target=cosine_sim_row, args=(row_data, mat_3, sims))
+    t4 = threading.Thread(target=cosine_sim_row, args=(row_data, mat_4, sims))
+
+    t1.start()
+    t1.join()
+    t2.start()
+    t2.join()
+    t3.start()
+    t3.join()
+    t4.start()
+    t4.join()
     end = time.time()
     print(f"Code ran in {end-start}s")
-    return sims
+    return [j for i in sims for j in i]
+
 
 
